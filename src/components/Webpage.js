faea0827e07d028f2d9c1d55c28f80e97bb3d66c
Shun Yao <ysmiracle@hotmail.com>
2018-06-03 21:29:37 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,10 +1,17 @@
 import FillerText from './FillerText';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 
 class Webpage extends React.Component {
+  text() {
+    return (
+      <title>The world's coolest webpage</title>
+    )
+  }
   render() {
     return (
-      
+      <FillerText />,
+      <FillerText />
+
     )
   }
 }
