4298ddae6af1b48850315fecd3a4c86a072b0200
Shun Yao <ysmiracle@hotmail.com>
2018-06-03 21:21:50 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,10 @@
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="password" />
+      </form>
+    )
+  }
+}
